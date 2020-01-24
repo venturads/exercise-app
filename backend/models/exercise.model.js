d75424172cc4c6c8c9d7ce175d921e5cec4e9fa6
venturads <albert@venturads.com>
2020-01-24 14:46:36 -0800

    added models and routes for user and exercises

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -6,7 +6,7 @@ const exerciseSchema = new Schema({
     username: { type: String, required: true },
     description: { type: String, required: true},
     duration: { type: Date, required: true },
-    date: { type: Date, required: true }
+    date: { type: Date, required: true },
 }, {
     timestamps: true,
 });
--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,20 +1,28 @@
 const router = require('express').Router();
-let User = require('../models/user.model');
+let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
-    User.find()
-        .then(users => res.json(users))
+    Exercise.find()
+        .then(exercises => res.json(exercises))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
     const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
 
-    const newUser = new User({username});
+    const newExercise = new Exercise({
+        username,
+        description,
+        duration,
+        date,
+    });
 
-    newUser.save()
-        .then(() => res.json('User added!'))
+    newExercise.save()
+        .then(() => res.json('Exercise added!'))
         .catch(err => res.status(400).json('Error:' + err));
-})
+});
 
 module.exports = router;
