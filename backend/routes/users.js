a44fd925d48eb7323995ac76a13437dfe41897e9
venturads <albert@venturads.com>
2020-01-25 21:32:55 -0800

    added users routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -16,4 +16,16 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req,res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
