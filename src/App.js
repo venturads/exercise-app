ba42f2dd7b171f3e1dfce5f29b2de0931c4d9269
venturads <albert@venturads.com>
2020-01-27 15:57:17 -0800

    added navbar component and folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+
+
 function App() {
   return (
     <Router>
