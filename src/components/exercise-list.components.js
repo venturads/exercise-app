6973576124601324fd5d578aa224d32639b137e9
venturads <albert@venturads.com>
2020-01-28 10:30:35 -0800

    create exercise list form

--- src/components/exercise-list.components.js
+++ src/components/exercise-list.components.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import { link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ExercisesList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+
+        this.state = { exercises: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/exercises/')
+            .then(response => {
+                this.setState({ exercises: response.data})
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    deleteExercise(id) {
+        axios.delete('http://localhost:5000/exercises/' + id)
+            .then(res => console.log(res.data));
+        
+        this.setState({
+            exercises: this.state.exercises.filter(el => el._id !== id)
+        })
+    }
+
     render() {
         return (
             <div>
                 <p>List Components!</p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.exercisesList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
