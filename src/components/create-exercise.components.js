dd57444dd8d40295b392fcb7334140b54e41610b
venturads <albert@venturads.com>
2020-01-27 21:37:07 -0800

    create state in exercise component

--- src/components/create-exercise.components.js
+++ src/components/create-exercise.components.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
 
 export default class ExercisesList extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDate(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
     render() {
         return (
             <div>
