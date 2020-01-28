6aa5ea1d6ee3025c719076ebb259cd05e9c5d4b7
venturads <albert@venturads.com>
2020-01-27 21:45:55 -0800

    bind this

--- src/components/create-exercise.components.js
+++ src/components/create-exercise.components.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 export default class ExercisesList extends Component {
     constructor(props) {
         super(props);
-        
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+
         this.state = {
             username: '',
             description: '',
@@ -12,6 +17,14 @@ export default class ExercisesList extends Component {
             users: []
         }
     }
+
+    componentDidMount(e) {
+        this.setState({
+            users: ['test user'],
+            username: 'test user'
+        })
+    }
+
     onChangeUsername(e) {
         this.setState({
             username: e.target.value
@@ -20,26 +33,41 @@ export default class ExercisesList extends Component {
 
     onChangeDescription(e) {
         this.setState({
-            username: e.target.value
+            description: e.target.value
         });
     }
 
     onChangeDuration(e) {
         this.setState({
-            username: e.target.value
+            duration: e.target.value
         });
     }
 
     onChangeDate(e) {
         this.setState({
-            username: e.target.value
+            date: date
         });
+    }
+
+    onSubmit() {
+        e.preventDefault();
+        
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercicse);
+
+        window.location = '/';
     }
 
     render() {
         return (
             <div>
-                <p>Create Components!</p>
+                
             </div>
         )
     }
